e60dd6decbb912ada74d967d8508bfb0389f658a
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-03-24 01:58:45 -0300

    db

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connect database
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "welcome to the contact keeper api" })
 );
+
+//define routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
