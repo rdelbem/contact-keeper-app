95b166619c0fe8f9186232c64f74478ce85a9970
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-03-28 01:57:37 -0300

    front end menu

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Navbar({ title, icon }) {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Navbar.propTypes = {
+  title: propTypes.string.isRequired,
+  icon: propTypes.string.isRequired,
+};
+
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt",
+};
+
+export default Navbar;
