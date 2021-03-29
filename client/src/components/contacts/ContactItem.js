0c86228e4d41e6f02fed56c363f919774fe93019
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-03-29 13:42:37 -0300

    delete contact from UI]

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import propTypes from "prop-types";
+import ContactContext from "../../context/contact/ContactContext";
 
 function ContactItem({ contact }) {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact } = contactContext;
+
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+  };
+
   return (
     <div className="card bg-light">
       <h3 className="text-primary text-left">
@@ -30,7 +39,9 @@ function ContactItem({ contact }) {
       </ul>
       <p>
         <button className="btn btn-dark btn-sm">Edit</button>
-        <button className="btn btn-danger btn-sm">Delete</button>
+        <button className="btn btn-danger btn-sm" onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   );
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -49,6 +49,9 @@ const ContactState = (props) => {
   };
 
   //delete contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   //set current contact
 
@@ -65,6 +68,7 @@ const ContactState = (props) => {
       value={{
         contacts: state.contacts,
         addContact,
+        deleteContact,
       }}
     >
       {props.children}
